e8e6e30a460dee4050847a4830680e389d6b6f8f
JRaphael <jraphaeloliveira14@gmail.com>
2021-06-16 09:53:20 -0300

    include fonts styles

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { SafeAreaView, Text, Touchable, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import styles from './styles';
 import LinearGradient from 'react-native-linear-gradient';
 import Input from '../../components/Input';
 
 import LoginImage from '../../assets/images/login_image.svg';
-import { useState } from 'react';
+import {useState} from 'react';
 
 import {useNavigation} from '@react-navigation/native';
 
 const SignIn = () => {
-
   const navigation = useNavigation();
 
   const [email, setEmail] = useState(String);
   const [password, setPassword] = useState(String);
+  const [message, setMessage] = useState(String);
+
+  const handleSignIn = () => {
+    if (email == "" || password == "")
+    {
+      setMessage('Insira todos os dados corretamente.')
+      return;
+    }
+  }
 
   return (
     <SafeAreaView style={styles.background}>
@@ -25,18 +39,24 @@ const SignIn = () => {
         style={styles.linearGradient}>
         <LoginImage />
 
-        <View style={styles.titleContainer}>
-          <Text style={styles.textTitle}>COVID ZONE</Text>
-        </View>
+        <KeyboardAvoidingView style={{flex: 1}} behavior='position'>
+          <View style={styles.titleContainer}>
+            <Text style={styles.textTitle}>COVID ZONE</Text>
+          </View>
 
-        <Input change={setEmail} placeholder="Digite seu email" value={email} />
+          <Input
+            change={setEmail}
+            placeholder="Digite seu email"
+            value={email}
+          />
 
-        <Input
-          placeholder="Digite sua senha"
-          value={password}
-          change={setPassword}
-          isPassword
-        />
+          <Input
+            placeholder="Digite sua senha"
+            value={password}
+            change={setPassword}
+            isPassword
+          />
+        </KeyboardAvoidingView>
       </LinearGradient>
 
       <View style={styles.buttonsContainer}>
@@ -57,7 +77,7 @@ const SignIn = () => {
           navigation.navigate('Dashboard');
         }}>
         <View style={styles.buttonLogin}>
-          <Text style={{color: '#fff'}}>Login</Text>
+          <Text style={styles.buttonLoginText}>Login</Text>
         </View>
       </TouchableWithoutFeedback>
     </SafeAreaView>
